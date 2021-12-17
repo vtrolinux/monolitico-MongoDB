2e6547c18c534bd64f5b3af13ce8a65ad40c7c27
vtrolinux <vtrolinux@gmail.com>
2021-12-17 07:26:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# monolitico-MongoDB
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+require('dotenv').config()
+
+const connection = require('./db/connection')
+
+//config handlebars
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.json())
+app.use(express.static('public'))
+//body parser, necessario para se trabalhar com forms
+app.use(express.urlencoded({ extended: true }))
+
