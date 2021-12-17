4a2f17c0771ff07b3c2f572163bb7ac80941b9de
vtrolinux <vtrolinux@gmail.com>
2021-12-17 19:14:34 -0300

    insert img

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,12 +1,12 @@
 const Product = require('../models/Product')
 
 module.exports = class ToughController {
+
   static async showProducts(req, res) {
-    //const products = await Product.getProducts()
+    const products = await Product.getProducts()
+    console.log(products)
 
-    //console.log(products)
-
-    res.render('products/all', /*{ products }*/)
+    res.render('products/all', { products })
   }
   static createProduct(req, res) {
     res.render('products/create')
@@ -15,8 +15,9 @@ module.exports = class ToughController {
     const name = req.body.name
     const price = req.body.price
     const description = req.body.description
+    const image = req.body.image
 
-    const product = new Product(name, price, description)
+    const product = new Product(name, price, description, image)
 
     product.save()
 
--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,24 @@
 const connection = require('../db/connection')
 
 class Product {
-    constructor(name, price, description){
+    constructor(name, price, description, image){
         this.name = name
         this.price = price
         this.description = description
+        this.image = image
     }
     save(){
         const product = connection.db().collection('products').insertOne({
             name: this.name,
             price: this.price,
-            description: this.description
+            description: this.description,
+            image: this.image
         })
         return product
+    }
+    static getProducts() {
+        const products = connection.db().collection('products').find().toArray()  
+        return products
     }
 }
 module.exports = Product
